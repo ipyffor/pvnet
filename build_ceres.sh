9f65b3e03e7b6b7ee1219ca130cf5d181598660c
gotpl <631707329@qq.com>
2020-12-27 14:46:31 +0800

    s

--- build_ceres.sh
+++ build_ceres.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ceres
 cd ceres
-wget http://ceres-solver.org/ceres-solver-1.14.0.tar.gz
+# wget http://ceres-solver.org/ceres-solver-1.14.0.tar.gz
 tar xvzf ceres-solver-1.14.0.tar.gz
 cd ceres-solver-1.14.0
 sed -i 's/\(^option(BUILD_SHARED_LIBS.*\)OFF/\1ON/' CMakeLists.txt
